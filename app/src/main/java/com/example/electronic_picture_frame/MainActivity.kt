38f4a560e864bfc4cbf515ce98c7d054ea35a1ef
polaris <61411085+polaris428@users.noreply.github.com>
2022-04-25 12:11:18 +0900

    사진 디렉토리 접근

--- app/src/main/java/com/example/electronic_picture_frame/MainActivity.kt
+++ app/src/main/java/com/example/electronic_picture_frame/MainActivity.kt
@@ -1,21 +1,36 @@
 package com.example.electronic_picture_frame
 
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
 
-    private val addPhotoButton:Button by lazy {
+    private val addPhotoButton: Button by lazy {
         findViewById(R.id.addPhotoButton)
     }
 
-    private val startPhotoFrameModeButton :Button by lazy {
+    private val startPhotoFrameModeButton: Button by lazy {
         findViewById(R.id.startPhotoFrameModeButton)
+    }
+    private val imageViewList: List<ImageView> by lazy {
+        mutableListOf<ImageView>().apply {
+            add(findViewById(R.id.imageView11))
+            add(findViewById(R.id.imageView12))
+            add(findViewById(R.id.imageView13))
+            add(findViewById(R.id.imageView21))
+            add(findViewById(R.id.imageView22))
+            add(findViewById(R.id.imageView23))
+
+        }
+
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,26 +41,52 @@ class MainActivity : AppCompatActivity() {
         initStartPhotoFrameModeButton()
     }
 
-    private  fun initAddPhotoButton(){
+    private fun initAddPhotoButton() {
         addPhotoButton.setOnClickListener {
-            when{
+            when {
                 ContextCompat.checkSelfPermission(
                     this,
                     android.Manifest.permission.READ_EXTERNAL_STORAGE
-                )==PackageManager.PERMISSION_GRANTED->{
-                    //권한이 잘 부여되었을때 갤러리에 사진을 선택하는 기능
+                ) == PackageManager.PERMISSION_GRANTED -> {
+                    navigatePhotos()
                 }
-                shouldShowRequestPermissionRationale( android.Manifest.permission.READ_EXTERNAL_STORAGE)->{
+                shouldShowRequestPermissionRationale(android.Manifest.permission.READ_EXTERNAL_STORAGE) -> {
                     showPermissionContextPopup()
                 }
-                else ->{
-                    requestPermissions(arrayOf( android.Manifest.permission.READ_EXTERNAL_STORAGE),1000)
+                else -> {
+                    requestPermissions(
+                        arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
+                        1000
+                    )
                 }
 
             }
         }
     }
-    private  fun   showPermissionContextPopup(){
+    private fun  navigatePhotos(){
+        val intent =Intent(Intent.ACTION_GET_CONTENT)
+        intent.type="image/*"
+        startActivityForResult(intent,2000)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when (requestCode){
+            1000->{
+                if(grantResults.isNotEmpty()&& grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                    navigatePhotos()
+                }else{
+                    Toast.makeText(this,"권한을 거부하였습니다",Toast.LENGTH_SHORT).show()
+                }
+            }else->{
+
+            }
+        }
+    }
+
+
+    private fun showPermissionContextPopup() {
         AlertDialog.Builder(this)
             .setTitle("권한이 필요합니다")
             .setMessage("전자액자에서 사진을 불러오기위해 권한이 필요합니다")
@@ -57,7 +98,7 @@ class MainActivity : AppCompatActivity() {
             .show()
     }
 
-    private  fun initStartPhotoFrameModeButton(){
+    private fun initStartPhotoFrameModeButton() {
 
     }
 
